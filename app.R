d56213a6b00e76f06e74e3b4e5433a8c40790c12
Edward Gillian <edwardjgillian@gmail.com>
2021-08-27 23:48:57 +0200

    update app.R

--- app.R
+++ app.R
@@ -5,14 +5,14 @@ library(geosphere)
 library(dplyr)
 
 # load the data
-shipsData <- readRDS("ships.rds")
+ships_data <- readRDS("ships.rds")
 
 # set up ships types and minimum and maximum latitude and longitude
-shipsTypes <- unique(shipsData["ship_type"])
-MIN_LAT <- min(shipsData["LAT"])
-MAX_LAT <- max(shipsData["LAT"])
-MIN_LON <- min(shipsData["LON"])
-MAX_LON <- max(shipsData["LON"])
+ships_types <- unique(ships_data["ship_type"])
+min_lat <- min(ships_data["LAT"])
+max_lat <- max(ships_data["LAT"])
+min_lon <- min(ships_data["LON"])
+max_lon <- max(ships_data["LON"])
 
 # Define UI for application that draws a histogram
 ui <- semanticPage(
@@ -27,14 +27,14 @@ ui <- semanticPage(
         sidebar_layout(
             sidebar_panel(
                 h1(selectInput(
-                    "shipTypeSelect",
+                    "ship_type_select",
                     label = "Ship Type:",
-                    choices = shipsTypes$ship_type,
-                    selected = shipsTypes$ship_type[1]
+                    choices = ships_types$ship_type,
+                    selected = ships_types$ship_type[1]
                 )
                 ),
-                h1(uiOutput("selectShipName")),
-                h2(textOutput("shipDistanceText")),
+                h1(uiOutput("select_ship_name")),
+                h2(textOutput("ship_distance_text")),
                 tags$br(),
             ),
             main_panel(
@@ -52,12 +52,11 @@ server <- function(input, output, session) {
 
         ranks <- order(df1$DATETIME)
 
-        nextLat <- dplyr::lead(df1[ranks, "LAT"], n = 1L, default = NA)
-        nextLon <- dplyr::lead(df1[ranks, "LON"], n = 1L, default = NA)
+        next_lat <- dplyr::lead(df1[ranks, "LAT"], n = 1L, default = NA)
+        next_lon <- dplyr::lead(df1[ranks, "LON"], n = 1L, default = NA)
 
 
-        data_list <- list(ranks = ranks, nextLat = nextLat, nextLon = nextLon)
-        names(data_list) <- c("ranks", "next_Lat", "next_Lon")
+        data_list <- list(ranks = ranks, next_lat = next_lat, next_lon = next_lon)
         return(data_list)
 
     }
@@ -71,45 +70,45 @@ server <- function(input, output, session) {
 
     values <- reactiveValues()
 
-    shipsFilteredByType <- reactive({
-        filteredShips <- shipsData[shipsData[, "ship_type"] == input$shipTypeSelect,]
-        values$shipName <- filteredShips[1, "SHIPNAME"]
-        filteredShips
+    ships_filtered_by_type <- reactive({
+        filtered_ships <- ships_data[ships_data[, "ship_type"] == input$ship_type_select,]
+        values$ship_name <- filtered_ships[1, "SHIPNAME"]
+        filtered_ships
     })
 
-    output$selectShipName <- renderUI({
-        shipsNames <- unique(shipsFilteredByType()["SHIPNAME"])
+    output$select_ship_name <- renderUI({
+        ships_names <- unique(ships_filtered_by_type()["SHIPNAME"])
         selectInput(
-            "shipName",
+            "ship_name",
             label = "Ship Name:",
-            choices = shipsNames$SHIPNAME,
-            selected = shipsNames$SHIPNAME[1]
+            choices = ships_names$SHIPNAME,
+            selected = ships_names$SHIPNAME[1]
         )
     })
 
-    observeEvent(input$shipName, {
-        req(input$shipName)
-        values$shipName <- input$shipName
+    observeEvent(input$ship_name, {
+        req(input$ship_name)
+        values$ship_name <- input$ship_name
     })
 
 
-    shipDistance <- reactive({
-        req(values$shipName, shipsFilteredByType())
+    ship_distance <- reactive({
+        req(values$ship_name, ships_filtered_by_type())
 
-        filteredByType <- shipsFilteredByType()
-        selectedShipData <- filteredByType[filteredByType[,"SHIPNAME"] == values$shipName,]
+        filtered_by_type <- ships_filtered_by_type()
+        selected_ship_data <- filtered_by_type[filtered_by_type[,"SHIPNAME"] == values$ship_name,]
 
         # create parameter list
-        parameter_list <- general_data_preprocess(selectedShipData)
+        parameter_list <- general_data_preprocess(selected_ship_data)
 
-        frameWithNextValues <- cbind(selectedShipData[parameter_list$ranks,], next_Lat = parameter_list$next_Lat, next_Lon = parameter_list$next_Lon)
-        frameWithoutNA <- frameWithNextValues[0:(nrow(frameWithNextValues)-1),]
+        frame_with_next_values <- cbind(selected_ship_data[parameter_list$ranks,], next_lat = parameter_list$next_lat, next_lon = parameter_list$next_lon)
+        frame_without_na <- frame_with_next_values[0:(nrow(frame_with_next_values)-1),]
 
-        distanceCalculation <- mapply(calculate_distance, frameWithoutNA$LON, frameWithoutNA$LAT, frameWithoutNA$next_Lon, frameWithoutNA$next_Lat)
+        distance_calculation <- mapply(calculate_distance, frame_without_na$LON, frame_without_na$LAT, frame_without_na$next_lon, frame_without_na$next_lat)
 
-        observationsIndexes <- which(distanceCalculation == max(distanceCalculation))
-        lastIndex <- tail(observationsIndexes, n=1)
-        cbind(frameWithoutNA[lastIndex,], distance=distanceCalculation[lastIndex])
+        observations_indexes <- which(distance_calculation == max(distance_calculation))
+        last_index <- tail(observations_indexes, n=1)
+        cbind(frame_without_na[last_index,], distance=distance_calculation[last_index])
     })
 
     output$map <- renderLeaflet({
@@ -121,31 +120,31 @@ server <- function(input, output, session) {
             return(addLegend(map, colors = colorAdditions, labels = labelAdditions, opacity = opacity))
         }
 
-        leaflet() %>%
-            addTiles() %>%
-            fitBounds(MIN_LON, MIN_LAT, MAX_LON, MAX_LAT) %>%
+        leaflet::leaflet() %>%
+            leaflet::addTiles() %>%
+            leaflet::fitBounds(min_lon, min_lat, max_lon, max_lat) %>%
             addLegendCustom(colors = c("orange", "blue"), labels = c("Start", "End"), sizes = c(10, 10))
     })
 
-    output$shipDistanceText <- renderText({
-        req(shipDistance())
-        paste("Ship distance in meters:", format(round(shipDistance()$distance, 0), nsmall = 0), sep=" ")
+    output$ship_distance_text <- renderText({
+        req(ship_distance())
+        paste("Ship distance in meters:", format(round(ship_distance()$distance, 0), nsmall = 0), sep=" ")
     })
 
     observe({
-        shipDistanceVector <- shipDistance()
+        ship_distance_vector <- ship_distance()
 
-        leafletProxy('map') %>% # use the proxy to save computation
-            clearShapes() %>%
-            addCircles(lng=c(shipDistanceVector$LON),
-                       lat=c(shipDistanceVector$LAT),
+        leaflet::leafletProxy('map') %>% # use the proxy to save computation
+            leaflet::clearShapes() %>%
+            leaflet::addCircles(lng=c(ship_distance_vector$LON),
+                       lat=c(ship_distance_vector$LAT),
                        group='circles',
                        weight=1, radius=100, color='orange',
                        fillColor='orange',
                        fillOpacity=0.5,
                        opacity=1) %>%
-            addCircles(lng=c(shipDistanceVector$next_Lon),
-                       lat=c(shipDistanceVector$next_Lat),
+            leaflet::addCircles(lng=c(ship_distance_vector$next_lon),
+                       lat=c(ship_distance_vector$next_lat),
                        group='circles',
                        weight=1, radius=100, color='blue',
                        fillColor='blue',
